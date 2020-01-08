8af418deba11b440f3f163d071eb78e1873522b3
Tania Banerjee <tania.mishra@gmail.com>
2020-01-08 13:50:32 -0500

    deleted un-needed files

--- online/test.py
+++
@@ -1,5 +0,0 @@
-def convertToPixelsPerSec(spkmph):
-    return 50 * spkmph / (3 * 2.23694)
-
-pps = convertToPixelsPerSec(60)
-print (pps * pps)
--- online/test.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash 
-
-rm ../7356/TrackProperties.csv
-python3 match.py ../7356/intersection3.txt
-python3 display.py ../7356/intersection3.txt
-diff ../7356/TrackProperties.csv ../7356/goldenTrackProperties.csv
